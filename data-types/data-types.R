b28c85f7933135fc3f8a222d16a70376d2a281d5
abrahamarslan <arslan@abrahamarslan.com>
2021-04-29 18:16:39 +0530

    #2 - Diamond Estimation and Data Types

---
+++ data-types/data-types.R
@@ -0,0 +1,22 @@
+#Integer variable
+x <- 2L
+typeof(x)
+
+#Float
+y <- 1.05
+typeof(y)
+
+#Complex
+z <- 3+2i
+typeof(z)
+
+#Character
+a <- "h"
+typeof(a)
+
+#Logical
+q1 <- T
+typeof(q1)
+
+q2 <- FALSE
+typeof(q2)
