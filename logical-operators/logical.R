1cfe300f629f398624cc2624b6cd6d72d590f2a7
abrahamarslan <arslan@abrahamarslan.com>
2021-04-30 00:33:17 +0530

    #3 - Logical Operators

---
+++ logical-operators/logical.R
@@ -0,0 +1,44 @@
+#Logical Variables
+
+# R has 10 logical operators
+
+# ==
+x <- 4==5
+x
+
+# != 
+x <- 5!=4
+x
+
+# <
+x <- 4<5
+x
+
+# >
+x <- 10>5
+x
+
+# <=
+x <- (1 <= 3)
+x
+
+# >=
+x <- (2 >= 5)
+x
+
+# !
+y <- !TRUE
+y
+
+# |
+z <- TRUE | TRUE
+z
+z <- TRUE | FALSE
+z
+
+# &
+z <- TRUE & FALSE
+z
+
+# isTrue(x)
+isTRUE(z)
---
+++ variables/variables.R
@@ -0,0 +1,19 @@
+#Varoab;es
+A <- 10 
+B <- 5
+C <- A + B
+C
+
+var1 <- 2.5
+var2 <- 4
+
+result <- var1 / var2
+result
+
+sqrtOfresult = sqrt(result)
+sqrtOfresult
+
+greeting <- "Hello"
+name <- "Bob"
+message <- paste(greeting, name)
+message
